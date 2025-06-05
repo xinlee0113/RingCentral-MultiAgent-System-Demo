d799bee8490478db0fea852369e779682ca5c0c8
Li Xin <xinlee0113@icloud.com>
2025-06-05 14:00:46 +0800

    更新简历，更新构建环境配置

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,11 +8,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.2")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.4")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-noarg:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:1.9.22")
     implementation("com.google.cloud.tools:jib-gradle-plugin:3.4.0")
-    implementation("org.sonarqube:org.sonarqube.gradle.plugin:4.4.1.3373")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.4.1.3373")
 } 
